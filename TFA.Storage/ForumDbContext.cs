d4264c2b79a2ae855456eaa830a93b7cd328bb48
Azat Kashapov <ichiru98@mail.ru>
2023-12-17 23:12:39 +0300

    init project

---
+++ TFA.Storage/ForumDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFA.Storage
+{
+    public class ForumDbContext : DbContext 
+    {
+        public ForumDbContext(DbContextOptions<ForumDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Forum> Forums { get; set; }
+        public DbSet<Topic> Topics { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+    }
+}
